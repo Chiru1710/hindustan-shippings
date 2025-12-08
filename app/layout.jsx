af6138ca9ebc27de9549d231b590afc727794745
Chiru1710 <107366845+Chiru1710@users.noreply.github.com>
2025-12-08 16:15:51 +0530

    Change body class for layout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,8 +17,8 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/favicon.svg" sizes="any" />
       </head>
-      <body className="antialiased text-white bg-blue-900">
-        <div className="flex flex-col min-h-screen px-6 sm:px-12">
+      <body className="antialiased text-neutral-50 bg-neutral-950">
+        <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
           <div className="flex flex-col w-full max-w-5xl mx-auto grow">
             <Header />
             <main className="grow">{children}</main>
@@ -29,3 +29,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
